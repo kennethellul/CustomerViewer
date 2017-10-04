99841461499d3d2f53933deb7c279c0793a35e2b
kenneth.ellul <kenneth.ellul@HWS356L0003>
2017-10-04 16:28:31 +0200

    Commiting first master branch project.

---
+++ CustomerViewer/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerViewer.Startup))]
+namespace CustomerViewer
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
