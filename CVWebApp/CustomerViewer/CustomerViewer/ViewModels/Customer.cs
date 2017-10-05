791cc0eddb91279f64e4437ff5fa8f57c1d283b8
alexia.falzon <falzon.alexia@gmail.com>
2017-10-05 13:50:48 +0200

    Table and button  GUI

---
+++ CVWebApp/CustomerViewer/CustomerViewer/ViewModels/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomerViewer.ViewModels
+{
+    public class Customer
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public String Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public String Gender { get; set; }
+    }
+}
